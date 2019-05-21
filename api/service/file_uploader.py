d4a0fbd42ebc7d50c8d9f8e1d852f0ddfecbaa72
unknown <sayakghosh.com@gmail.com>
2019-05-21 17:30:55 -0400

    Added fix

--- api/service/file_uploader.py
+++ api/service/file_uploader.py
@@ -8,10 +8,10 @@ def upload_file(user_id, uploaded_file):
     user = User.query.filter(User.id == user_id).one()
 
     if uploaded_file:
-        destination = os.path.join(settings.DATA_PATH, 'data\\' + user.username)
+        destination = os.path.join(settings.DATA_PATH, 'data/' + user.username)
         if not os.path.exists(destination):
             os.makedirs(destination)
-        file_path = '%s%s' % (destination, uploaded_file.filename)
+        file_path = '%s/%s' % (destination, uploaded_file.filename)
 
         uploaded_file.save(file_path)
         filename = uploaded_file.filename
